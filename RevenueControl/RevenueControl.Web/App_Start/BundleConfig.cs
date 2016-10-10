9d9ca96241143c4b1e99fdcbd35dd870f93f035c
Iulian Holonca <Iulian@IULIAN-PC>
2016-10-10 21:22:06 +0300

    Removed jquery-ui

--- RevenueControl/RevenueControl.Web/App_Start/BundleConfig.cs
+++ RevenueControl/RevenueControl.Web/App_Start/BundleConfig.cs
@@ -7,47 +7,25 @@ namespace RevenueControl.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/revctrl").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery-ui-{version}.js",
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
+                       "~/Scripts/jquery.unobtrusive*",
+                       "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/core.css",
-                "~/Content/themes/base/resizable.css",
-                "~/Content/themes/base/selectable.css",
-                "~/Content/themes/base/accordion.css",
-                "~/Content/themes/base/autocomplete.css",
-                "~/Content/themes/base/button.css",
-                "~/Content/themes/base/dialog.css",
-                "~/Content/themes/base/slider.css",
-                "~/Content/themes/base/tabs.css",
-                "~/Content/themes/base/datepicker.css",
-                "~/Content/themes/base/progressbar.css",
-                "~/Content/themes/base/theme.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
         }
     }
 }
--- RevenueControl/RevenueControl.Web/Scripts/_references.js
+++ RevenueControl/RevenueControl.Web/Scripts/_references.js
@@ -6,7 +6,5 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.12.4.js" />
 /// <reference path="jquery-1.12.4.min.js" />
-/// <reference path="jquery-ui-1.12.1.js" />
-/// <reference path="jquery-ui-1.12.1.min.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
